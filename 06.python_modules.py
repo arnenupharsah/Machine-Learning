248ee68286ab1af0beee0eac5c3126497a586572
arnenupharsah <arnenupharsah@gmail.com>
2019-07-13 10:30:41 +0530

    Completed Python

---
+++ 06.python_modules.py
@@ -0,0 +1,13 @@
+import math
+#from math import factorial as fact
+
+print(math.floor(15/2))
+print(math.gcd(12,6))
+
+print(math.log(math.e))
+
+print(math.pi)
+print(math.inf)
+print(-math.inf)
+print(sum([1,2,3,4,5]))
+print(max([1,2,3,4,5,0]))
